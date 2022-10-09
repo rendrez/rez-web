5ed3e85985e604e81757fe61d674576dbb396bb7
Rendrez <rendrez0027@gmail.com>
2022-10-09 11:40:30 +0700

    menambah navbar dan background image

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
+import bg from './bg.jpg'
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>My first reactjs</h1>
-        <p>Updates soon</p>
-      </header>
+      <div className='background'>
+        <Navbar />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Muhammad Randy</h1>
+          <p>I'm currently learning about Reactjs</p>
+        </header>
+      </div>
     </div>
   );
 }
+
+// Background photo by <a href="https://unsplash.com/@dell?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dell</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
 
 export default App;
